1b2db518a7ac61622484f3dc81055a7a1d6e75e9
Igor de Oliveira Corrêa <igocorrea@gmail.com>
2019-04-29 19:06:57 -0300

    constructors e get/set
    Adição do constructor da classe principal
    Adição do get/set da classe principal

--- src/main/java/employltda/domain/Booking.java
+++ src/main/java/employltda/domain/Booking.java
@@ -17,17 +17,119 @@ public class Booking{
     private List bookingRoomID;
 
     public Booking(){
-
-        startDate = " ";
-        endDate = " ";
-        bookingResponsible = " ";
-        note = " ";
-
-        isCoffee = false;
-
-        bookingLocalID = new ArrayList<>();
-        bookingRoomID = new ArrayList<>();
     }
+
+    public Booking(String startDate, String endDate, String bookingResponsible,
+    String note, Boolean isCoffee, List bookingLocalID, List bookingRoomID){
+        super();
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.bookingResponsible = bookingResponsible;
+        this.note = note;
+        this.isCoffee = isCoffee;
+        this.bookingLocalID = bookingLocalID;
+        this.bookingRoomID = bookingRoomID;
+    }
+
+    /**
+     * @return the startDate
+     */
+    public String getStartDate() {
+        return startDate;
+    }
+
+    /**
+     * @param startDate the startDate to set
+     */
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    /**
+     * @return the endDate
+     */
+    public String getEndDate() {
+        return endDate;
+    }
+
+    /**
+     * @param endDate the endDate to set
+     */
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    /**
+     * @return the bookingResponsible
+     */
+    public String getBookingResponsible() {
+        return bookingResponsible;
+    }
+
+    /**
+     * @param bookingResponsible the bookingResponsible to set
+     */
+    public void setBookingResponsible(String bookingResponsible) {
+        this.bookingResponsible = bookingResponsible;
+    }
+
+    /**
+     * @return the note
+     */
+    public String getNote() {
+        return note;
+    }
+
+    /**
+     * @param note the note to set
+     */
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    /**
+     * @return the isCoffee
+     */
+    public Boolean getIsCoffee() {
+        return isCoffee;
+    }
+
+    /**
+     * @param isCoffee the isCoffee to set
+     */
+    public void setIsCoffee(Boolean isCoffee) {
+        this.isCoffee = isCoffee;
+    }
+
+    /**
+     * @return the bookingLocalID
+     */
+    public List getBookingLocalID() {
+        return bookingLocalID;
+    }
+
+    /**
+     * @param bookingLocalID the bookingLocalID to set
+     */
+    public void setBookingLocalID(List bookingLocalID) {
+        this.bookingLocalID = bookingLocalID;
+    }
+
+    /**
+     * @return the bookingRoomID
+     */
+    public List getBookingRoomID() {
+        return bookingRoomID;
+    }
+
+    /**
+     * @param bookingRoomID the bookingRoomID to set
+     */
+    public void setBookingRoomID(List bookingRoomID) {
+        this.bookingRoomID = bookingRoomID;
+    }
+
+    
 
 
 
